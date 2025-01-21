4b858bc20e8999654b60014d86144ab3f27a166b
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2025-01-21 14:08:51 +0100

    styling

--- template/src/app/page.tsx
+++ template/src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import Panel0 from "./components/panel0";
 import Panel1 from "./components/panel1";
 import Panel2 from "./components/panel2";
@@ -6,23 +8,53 @@ import Panel3 from "./components/panel3";
 // # this is branch main2
 
 export default function Dashboard() {
+  const [isScrollable, setIsScrollable] = useState(true);
+
+  const toggleScrollable = () => {
+    setIsScrollable(!isScrollable);
+  };
+
   return (
-    <div className="bg-stone-100 w-full h-screen p-4 flex flex-col">
-      <div className="p-6 bg-white border border-gray-200 rounded-xl mx-auto w-full max-w-screen-xl flex-1 flex flex-col">
-        <h1 className="text-xl font-semibold pb-4">Dashboard Overview</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-3 flex-1">
-          <div className="rounded-xl border-2 border-gray-200 h-full p-4">
-            <Panel0 />
-          </div>
-          <div className="rounded-xl border-2 border-gray-200 h-full p-4">
-            <Panel1 />
-          </div>
-          <div className="rounded-xl border-2 border-gray-200 h-full p-4">
-            <Panel2 />
-          </div>
-          <div className="rounded-xl border-2 border-gray-200 h-full p-4">
-            <Panel3 />
-          </div>
+    <div className="w-screen h-screen p-4">
+      <div className="flex h-[5%] justify-between items-center mb-2 ">
+        <h1>Dashboard</h1>
+        <button
+          onClick={toggleScrollable}
+          className={`p-2 border-[1px] rounded ${
+            isScrollable ? "bg-blue-500 text-white" : "text-black bg-white border-black"
+          }`}
+        >
+          Toggle Scrollable
+        </button>
+      </div>
+      <div className="w-full h-[95%] grid grid-cols-2 grid-rows-2 gap-3">
+        <div
+          className={`rounded-xl border-2 border-gray-200 ${
+            isScrollable ? "overflow-auto" : "overflow-hidden"
+          }`}
+        >
+          <Panel0 />
+        </div>
+        <div
+          className={`rounded-xl border-2 border-gray-200 ${
+            isScrollable ? "overflow-auto" : "overflow-hidden"
+          }`}
+        >
+          <Panel1 />
+        </div>
+        <div
+          className={`rounded-xl border-2 border-gray-200 ${
+            isScrollable ? "overflow-auto" : "overflow-hidden"
+          }`}
+        >
+          <Panel2 />
+        </div>
+        <div
+          className={`rounded-xl border-2 border-gray-200 ${
+            isScrollable ? "overflow-auto" : "overflow-hidden"
+          }`}
+        >
+          <Panel3 />
         </div>
       </div>
     </div>
