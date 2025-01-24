a37ee2fff57550f4db1c02f83ee2dfb29bca3372
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2025-01-24 16:44:40 +0100

    vercel deploy test2

--- template/next.config.ts
+++ template/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   ignoreBuildErrors: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
