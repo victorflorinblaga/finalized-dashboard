69a41bb9eeedd7ab82801703ad703eb437b036ee
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2025-01-27 13:34:23 +0100

    creating file: template/src/queries/generated/othertest@gmail.com/m6f11fwyuptoa7gyw7r/query.ts

---
+++ template/src/queries/generated/othertest@gmail.com/m6f11fwyuptoa7gyw7r/query.ts
@@ -0,0 +1,15 @@
+
+  export const query = `
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX co2: <http://example.com/>
+
+SELECT ?population ?country ?year ?gdp
+WHERE {
+    ?record a co2:EmissionRecord;
+            co2:population ?population;
+            co2:country ?country;
+            co2:year ?year;
+            co2:gdp ?gdp.
+    FILTER (?year >= "2000"^^xsd:gYear && ?year <= "2020"^^xsd:gYear).
+}
+`;
