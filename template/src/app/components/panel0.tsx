ee4e130ef88bf3bf2672892311664cc6bb47fbd6
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2025-02-10 12:23:28 +0100

    updating contents of template/src/app/components/panel0.tsx

--- template/src/app/components/panel0.tsx
+++ template/src/app/components/panel0.tsx
@@ -1,19 +1,21 @@
 "use client";
 
 import React from "react"
+import { useEffect, useState } from "react";
 import { useQuery } from "@/hooks/useQuery";
 import LoadingIndicator from "@/components/LoadingIndicator"
-import { query } from "@/queries/generated/othertest@gmail.com/m6uvfyg5tskdrg0g3s/query";
+import { query } from "@/queries/generated/othertest@gmail.com/m6yyq3ei9ngtyhojpih/query";
 import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-} from "@/components/ui/card";
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
-const url = "http://genui-kg-a8hedtafhpb0fwak.germanywestcentral-01.azurewebsites.net/repositories/sustainability";
+const url = "http://genui-kg-a8hedtafhpb0fwak.germanywestcentral-01.azurewebsites.net/repositories/purchasing";
 
 export default function Page() {
   
@@ -21,29 +23,27 @@ export default function Page() {
 
   if (loading) return <LoadingIndicator />;
 
-  const germanyImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/2560px-Flag_of_Germany.svg.png";
-  const franceImageUrl = "https://upload.wikimedia.org/wikipedia/en/c/c3/Flag_of_France.svg";
-
   return (
-    <div className="w-full h-full p-4 flex flex-col items-center justify-center space-y-4">
-      <Card className="w-full md:w-1/2 lg:w-1/3">
-        <CardHeader>
-          <CardTitle className="text-xl font-bold">Comparison Germany and France</CardTitle>
-          <CardDescription className="text-sm">A detailed analysis of data from Germany and France.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="flex justify-center space-x-4">
-            <Avatar>
-              <AvatarImage src={germanyImageUrl} />
-              <AvatarFallback>Germany Flag</AvatarFallback>
-            </Avatar>
-            <Avatar>
-              <AvatarImage src={franceImageUrl} />
-              <AvatarFallback>France Flag</AvatarFallback>
-            </Avatar>
-          </div>
-        </CardContent>
-      </Card>
+    <div className="size-full p-2 overflow-x-auto">
+      <Table>
+        <TableCaption>A list of user information.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            {headers.map((header, index) => (
+              <TableHead key={index} className="w-auto">{header}</TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {rows.map((row, rowIndex) => (
+            <TableRow key={rowIndex}>
+              {row.map((cell, cellIndex) => (
+                <TableCell key={cellIndex} className="font-medium">{cell}</TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   )
 }
