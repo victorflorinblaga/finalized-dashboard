1bef9f528776c88aa2faabb26a3e18e259f88872
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2025-02-18 10:29:21 +0100

    updating contents of template/src/app/components/panel0.tsx

--- template/src/app/components/panel0.tsx
+++ template/src/app/components/panel0.tsx
@@ -1,20 +1,10 @@
 "use client";
 
 import React from "react"
+import { useEffect, useState } from "react";
 import { useQuery } from "@/hooks/useQuery";
 import LoadingIndicator from "@/components/LoadingIndicator"
-import { query } from "@/queries/generated/othertest@gmail.com/m6z2egianmd8ctftrh/query";
-import { Bar } from 'react-chartjs-2';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-
-ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip, Legend);
+import { query } from "@/queries/generated/othertest@gmail.com/m7a8dfvctb868anveyc/query";
 
 const url = "http://genui-kg-a8hedtafhpb0fwak.germanywestcentral-01.azurewebsites.net/repositories/purchasing";
 
@@ -24,25 +14,9 @@ export default function Page() {
 
   if (loading) return <LoadingIndicator />;
 
-  const labels = rows.map(row => row[0]); // assuming first column is the label
-  const dataValues = rows.map(row => parseInt(row[1], 10)); // assuming second column is the value
-
-  const barData = {
-    labels: labels,
-    datasets: [
-      {
-        label: 'RFQ Volume',
-        backgroundColor: 'rgba(75,192,192,1)',
-        borderColor: 'rgba(255,255,255)',
-        borderWidth: 2,
-        data: dataValues,
-      },
-    ],
-  };
-
   return (
-    <div className="w-full h-full p-4">
-      <Bar data={barData} options={{ responsive: true }} height={400} />
+    <div className="w-full h-full p-2 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-center">Example Headline</h1>
     </div>
-  );
+  )
 }
