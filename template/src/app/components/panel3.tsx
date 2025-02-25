ef10206a23b0d71deead5950c4ec7a0852076ec9
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2025-02-25 09:00:02 +0100

    updating contents of template/src/app/components/panel3.tsx

--- template/src/app/components/panel3.tsx
+++ template/src/app/components/panel3.tsx
@@ -1,20 +1,12 @@
 "use client";
 
-import React from "react";
+import React from "react"
+import { useEffect, useState } from "react";
 import { useQuery } from "@/hooks/useQuery";
-import LoadingIndicator from "@/components/LoadingIndicator";
-import { query } from "@/queries/generated/othertest@gmail.com/m7dhysvp6ty4ttupn85/query";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+import LoadingIndicator from "@/components/LoadingIndicator"
+import { query } from "@/queries/generated/othertest@gmail.com/m7a8dfvctb868anveyc/query";
 
-const url = "http://genui-kg-a8hedtafhpb0fwak.germanywestcentral-01.azurewebsites.net/repositories/sustainability";
+const url = "http://genui-kg-a8hedtafhpb0fwak.germanywestcentral-01.azurewebsites.net/repositories/purchasing";
 
 export default function Page() {
   
@@ -22,30 +14,9 @@ export default function Page() {
 
   if (loading) return <LoadingIndicator />;
 
-  const germanyRows = rows.filter(row => row[0] === "Germany");
-  const lowestGdpRow = germanyRows.reduce((prev, curr) => {
-    return parseFloat(prev[3]) < parseFloat(curr[3]) ? prev : curr;
-  });
-
   return (
-    <div className="w-full h-full p-2 overflow-x-auto">
-      <Table>
-        <TableCaption>Year with the lowest GDP for Germany.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            {headers.map((header, index) => (
-              <TableHead key={index}>{header}</TableHead>
-            ))}
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            {lowestGdpRow.map((cell, cellIndex) => (
-              <TableCell key={cellIndex}>{cell}</TableCell>
-            ))}
-          </TableRow>
-        </TableBody>
-      </Table>
+    <div className="w-full h-full p-2 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-center">Example Headline</h1>
     </div>
-  );
+  )
 }
