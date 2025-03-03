7f128c48367b433c6a0a7c268fff7f422fd26a80
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2025-03-03 10:40:37 +0100

    creating file: template/src/queries/generated/selam.geg@yahoo.com/m7sv0dnzjyz41lq5u6/query.ts

---
+++ template/src/queries/generated/selam.geg@yahoo.com/m7sv0dnzjyz41lq5u6/query.ts
@@ -0,0 +1,13 @@
+
+  export const query = `
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX ex: <http://example.com/>
+
+SELECT ?rfq ?rfq_volume ?rfq_close_date
+WHERE {
+    ?rfq a ex:rfq;
+         ex:rfq_volume ?rfq_volume;
+         ex:rfq_close_date ?rfq_close_date.
+    FILTER (YEAR(?rfq_close_date) = 2024)
+}
+`;
