3643f9931bcf77cd202e32a3f414dbc397f98ea3
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2025-03-03 15:58:34 +0100

    updating contents of template/src/app/components/panel3.tsx

--- template/src/app/components/panel3.tsx
+++ template/src/app/components/panel3.tsx
@@ -4,16 +4,7 @@ import React from "react"
 import { useEffect, useState } from "react";
 import { useQuery } from "@/hooks/useQuery";
 import LoadingIndicator from "@/components/LoadingIndicator"
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { query } from "@/queries/generated/selam.geg@yahoo.com/m7stjdcszmd76yee6y/query";
+import { query } from "@/queries/generated/othertest@gmail.com/m7a8dfvctb868anveyc/query";
 
 const url = "http://genui-kg-a8hedtafhpb0fwak.germanywestcentral-01.azurewebsites.net/repositories/purchasing";
 
@@ -24,26 +15,8 @@ export default function Page() {
   if (loading) return <LoadingIndicator />;
 
   return (
-    <div className="w-full h-full p-2">
-      <Table>
-        <TableCaption>A list of employee data 📋</TableCaption>
-        <TableHeader>
-          <TableRow>
-            {headers.map((header, index) => (
-              <TableHead key={index} className="text-left">{header} 🡕</TableHead>
-            ))}
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {rows.map((row, rowIndex) => (
-            <TableRow key={rowIndex}>
-              {row.map((cell, cellIndex) => (
-                <TableCell key={cellIndex} className="text-left">{cell} 📞</TableCell>
-              ))}
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+    <div className="w-full h-full p-2 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-center">Example Headline</h1>
     </div>
   )
 }
