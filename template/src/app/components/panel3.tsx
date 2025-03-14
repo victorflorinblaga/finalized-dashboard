efd73943a713870415bdceb4eaa6b7466861f7c3
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2025-03-14 11:37:14 +0100

    updating contents of template/src/app/components/panel3.tsx

--- template/src/app/components/panel3.tsx
+++ template/src/app/components/panel3.tsx
@@ -4,19 +4,45 @@ import React from "react"
 import { useEffect, useState } from "react";
 import { useQuery } from "@/hooks/useQuery";
 import LoadingIndicator from "@/components/LoadingIndicator"
-import { query } from "@/queries/generated/othertest@gmail.com/m7a8dfvctb868anveyc/query";
+import { query } from "@/queries/generated/othertest@gmail.com/m88lzxuxmi9alagolv/query";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
-const url = "http://genui-kg-a8hedtafhpb0fwak.germanywestcentral-01.azurewebsites.net/repositories/purchasing";
+const url = "http://genui-kg-a8hedtafhpb0fwak.germanywestcentral-01.azurewebsites.net/repositories/sustainability";
 
 export default function Page() {
-  
   const [headers, rows, loading] = useQuery(url, query);
 
   if (loading) return <LoadingIndicator />;
 
   return (
-    <div className="w-full h-full p-2 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-center">Example Headline</h1>
+    <div className="w-full h-full p-2">
+      <Table>
+        <TableCaption>A list of countries' population data 🌍</TableCaption>
+        <TableHeader>
+          <TableRow>
+            {headers.map((header, index) => (
+              <TableHead key={index} className="w-[100px]">{header}</TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {rows.map((row, rowIndex) => (
+            <TableRow key={rowIndex}>
+              {row.map((cell, cellIndex) => (
+                <TableCell key={cellIndex} className="font-medium">{cell} 📊</TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   )
 }
