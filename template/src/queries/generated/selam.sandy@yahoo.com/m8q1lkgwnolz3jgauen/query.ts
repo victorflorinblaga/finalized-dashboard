04e862b16e9b7b4f135a15e74ac85705bdcdb8a1
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2025-03-26 16:13:35 +0100

    creating file: template/src/queries/generated/selam.sandy@yahoo.com/m8q1lkgwnolz3jgauen/query.ts

---
+++ template/src/queries/generated/selam.sandy@yahoo.com/m8q1lkgwnolz3jgauen/query.ts
@@ -0,0 +1,13 @@
+
+  export const query = `
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX ex: <http://example.com/>
+
+SELECT ?project ?project_title ?project_status ?project_start_date
+WHERE {
+    ?project a ex:project;
+              ex:project_title ?project_title;
+              ex:project_status ?project_status;
+              ex:project_start_date ?project_start_date.
+}
+`;
