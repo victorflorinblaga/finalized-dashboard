c7bae8c3298e50ae662c09fca2d1d3d94f0087b5
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2025-04-03 08:16:20 +0200

    updating contents of template/src/app/components/panel1.tsx

--- template/src/app/components/panel1.tsx
+++ template/src/app/components/panel1.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import React from "react";
+import React from "react"
 import { useQuery } from "@/hooks/useQuery";
-import LoadingIndicator from "@/components/LoadingIndicator";
-import { query } from "@/queries/generated/selam.geg@yahoo.com/m8x1e280rw9f7hhnxd/query";
+import LoadingIndicator from "@/components/LoadingIndicator"
+import { query } from "@/queries/generated/selam.geg@yahoo.com/m90xh4y5078zwix7qm1e/query";
 import {
   Table,
   TableBody,
@@ -14,44 +14,40 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
-const url = "http://genui-kg-a8hedtafhpb0fwak.germanywestcentral-01.azurewebsites.net/repositories/sustainability";
+const url = "http://genui-kg-a8hedtafhpb0fwak.germanywestcentral-01.azurewebsites.net/repositories/purchasing";
 
 export default function Page() {
+  
   const [headers, rows, loading] = useQuery(url, query);
 
   if (loading) return <LoadingIndicator />;
 
-  const germanyRows = rows.filter(row => row[0] === "Germany").sort((a, b) => Number(a[2]) - Number(b[2]));
-
   return (
-    <div className="w-full h-full">
-      <div className="p-4">
-        <Table>
-          <TableCaption>A table of sustainability data for Germany, sorted by GDP.</TableCaption>
-          <TableHeader>
-            <TableRow>
-              {headers.map((header, index) => (
-                <TableHead key={index} className="w-[100px]">
-                  {header}
-                </TableHead>
-              ))}
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {germanyRows.map((row, index) => (
-              <TableRow key={index} className={row[1] === "2008" ? "bg-yellow-200" : ""}>
+    <div className="w-full h-full p-2 overflow-x-auto">
+      <Table>
+        <TableCaption>A list of projects.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            {headers.map((header, index) => (
+              <TableHead key={index}>{header}</TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {rows.map((row, index) => {
+            const projectTitle = row[1].replace(/^http:\/\/example\.com\//, "");
+            return (
+              <TableRow key={index} className={row[2] === "In Progress" ? "bg-green-200" : ""}>
                 {row.map((cell, cellIndex) => (
-                  <TableCell key={cellIndex} className="p-2">
-                    {cellIndex === 2 ? `${Number(cell).toLocaleString()} USD` : 
-                     cellIndex === 3 ? `${Number(cell).toLocaleString()} people` : 
-                     cell}
+                  <TableCell key={cellIndex}>
+                    {cellIndex === 1 ? projectTitle : cell}
                   </TableCell>
                 ))}
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
+            );
+          })}
+        </TableBody>
+      </Table>
     </div>
-  );
+  )
 }
