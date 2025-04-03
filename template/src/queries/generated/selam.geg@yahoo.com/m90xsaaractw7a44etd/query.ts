e8b23a0c92c1957296d78b1e6a3eff4bfc8f1a0d
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2025-04-03 08:16:02 +0200

    creating file: template/src/queries/generated/selam.geg@yahoo.com/m90xsaaractw7a44etd/query.ts

---
+++ template/src/queries/generated/selam.geg@yahoo.com/m90xsaaractw7a44etd/query.ts
@@ -0,0 +1,12 @@
+
+  export const query = `
+PREFIX ex: <http://example.com/>
+
+SELECT ?supplier ?supplier_name ?supplier_pyramid_rank ?supplier_is_top_parent ?supplier_address
+WHERE {
+    ?supplier ex:supplier_name ?supplier_name;
+              ex:supplier_pyramid_rank ?supplier_pyramid_rank;
+              ex:supplier_is_top_parent ?supplier_is_top_parent;
+              ex:supplier_address ?supplier_address.
+}
+`;
