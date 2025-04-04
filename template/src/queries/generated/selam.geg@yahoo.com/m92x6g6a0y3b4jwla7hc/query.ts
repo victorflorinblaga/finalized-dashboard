1171ca9d48657840f2c1d3dde223b1c8485b262a
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2025-04-04 17:20:49 +0200

    creating file: template/src/queries/generated/selam.geg@yahoo.com/m92x6g6a0y3b4jwla7hc/query.ts

---
+++ template/src/queries/generated/selam.geg@yahoo.com/m92x6g6a0y3b4jwla7hc/query.ts
@@ -0,0 +1,17 @@
+
+  export const query = `
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX co2: <http://example.com/>
+
+SELECT ?country ?year ?population ?co2 ?cement_co2_per_capita
+WHERE {
+    ?record a co2:EmissionRecord;
+            co2:country ?country;
+            co2:year ?year;
+            co2:population ?population;
+            co2:co2 ?co2;
+            co2:cement_co2_per_capita ?cement_co2_per_capita.
+    FILTER (?country IN ("China", "Russia", "India", "Japan")).
+    FILTER (?year >= "2000"^^xsd:gYear && ?year <= "2020"^^xsd:gYear).
+}
+`;
